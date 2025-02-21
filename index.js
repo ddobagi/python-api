524d62c5a095f9d131c60fb6425a54135ddcefd8
ddobagi <freshcom@snu.ac.kr>
2025-02-21 14:26:09 +0900

    Add .gitattributes to ormalize line endings

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/run-python', (req, res) => {
+  exec('python script.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return res.status(500).json({ error: 'Python script error' });
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return res.status(500).json({ error: 'Python script stderr' });
+    }
+    
+    console.log(`Python Output: ${stdout.trim()}`); // Python 출력 로그
+    res.json({ result: stdout.trim() });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
